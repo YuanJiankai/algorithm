2f8b55c3f9aa903187f5f77c0ee8b3709a64c485
yuanjiankai <735377503@qq.com>
2018-03-14 23:43:43 +0800

    加了一点 等会完成后头部分

--- src/hard/problem_4_1.java
+++ src/hard/problem_4_1.java
@@ -1,4 +1,18 @@
 package hard;
 
 public class problem_4_1 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double result = 0.0;
+        if(nums1.length + nums2.length == 0) {
+            return result;
+        }
+        if(nums1.length == 0) {
+            return (double) (nums2.length % 2 == 0 ? nums2[nums2.length / 2 + nums2.length / 2 - 1] : nums2[nums2.length / 2]);
+        } else if(nums2.length == 0) {
+            return (double) (nums1.length % 2 == 0 ? nums1[nums1.length / 2 + nums1.length / 2 - 1] : nums1[nums1.length / 2]);
+        }
+
+        
+
+    }
 }
